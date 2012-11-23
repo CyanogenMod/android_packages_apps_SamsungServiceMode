c8766552d7af6d29445f8224df989d4ffa3b1aed
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-11-23 12:33:20 +0700

    SamsungServiceMode: add required telephony-common library
    Required in jellybean 4.2
    
    Change-Id: Ibe5468a97a6bb49930ef5ae10fecc60078022664

--- Android.mk
+++ Android.mk
@@ -8,6 +8,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := SamsungServiceMode
+LOCAL_JAVA_LIBRARIES := telephony-common
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
